2367f0ce61a3c60d050f7b387cda9951363b49af
Satont <satontworldwide@gmail.com>
2020-11-02 01:09:16 +0300

    Remove console.log

--- src/pics/pics.service.ts
+++ src/pics/pics.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common'
 export class PicsService {
   getPics(page: number): string[] {
     const number = page + 50
-    console.log(number)
+
     const pics = Array(number).fill('https://via.placeholder.com/200.png')
     return pics
   }
