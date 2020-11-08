17fc11b269b1809e65ffd9fd888c421afead66d5
Satont <satontworldwide@gmail.com>
2020-11-08 10:50:59 +0300

    upd

--- src/web/pages/pics.vue
+++ src/web/pages/pics.vue
@@ -8,21 +8,27 @@
         </b-nav>
       </div>
       <div class="container d-flex h-100 align-items-center">
-        <div v-for="img in list" :key="img.src" class="col-sm-3 pb-2">
-          <img
-            :src="img.fileUrl"
-            class="list-pic"
-            data-toggle="modal"
-            data-target="#imgModal"
-            :data-id="img.id"
-            :data-name="img.name"
-            :data-createdat="img.createdAt"
-            :data-updatedat="img.updatedAt"
-            @click="selected = img; $bvModal.show('bv-modal-example')"
-          />
+        <div class="row">
+          <div v-for="item in list" :key="item.id" class="col-sm-3 pb-2">
+            <img
+              :src="item.fileUrl"
+              class="list-pic"
+              data-toggle="modal"
+              data-target="#imgModal"
+              :data-id="item.id"
+              :data-name="item.name"
+              :data-createdat="item.createdAt"
+              :data-updatedat="item.updatedAt"
+              @click="selected = item; $bvModal.show('bv-modal-example')"
+            />
+          </div>
         </div>
       </div>
     </section>
+
+    <infinite-loading @infinite="onInfinity" :identifier="infiniteId" spinner="waveDots">
+      <div class="text-white-50" slot="no-more">No more pics. :(</div>
+    </infinite-loading>
 
     <b-modal id="bv-modal-example" hide-footer>
       <template #modal-title>
@@ -42,29 +48,47 @@
 
 <script lang='ts'>
 import { Vue, Component, Watch } from 'vue-property-decorator'
+import InfiniteLoading from 'vue-infinite-loading'
 
-@Component
+@Component({
+  components: {
+    InfiniteLoading,
+  }
+})
 export default class App extends Vue {
+  loading = false
   page = 1
   category = 'general'
   list = []
   selected = {}
+  infiniteId = +new Date()
 
-  async mounted() {
-    this.getPics()
+  async onInfinity($state) {
+    const pics = await this.getPics()
+
+    if (pics.length) {
+      this.page++
+      this.list.push(...pics)
+      $state.loaded()
+    } else {
+      $state.complete()
+    }
   }
 
   async getPics() {
     const request = await fetch(`/api/pics?page=${this.page}&category=${this.category}`)
     const { pics } = await request.json()
 
-    this.list = pics
+    return pics
   }
 
   @Watch('category')
   onCategoryChange(val: string, oldVal: string) {
     if (val === oldVal) return
-    this.getPics()
+
+    this.page = 1
+    this.list = []
+    this.infiniteId += 1
   }
 }
 </script>
