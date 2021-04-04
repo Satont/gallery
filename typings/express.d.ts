ec58846de3860907815fcab82f63c6d1808f598c
Satont <satontworldwide@gmail.com>
2021-04-04 04:39:45 +0300

    Make category general as default in UI

--- typings/express.d.ts
+++ typings/express.d.ts
@@ -1,19 +1,7 @@
-import { User as DiscordJsUser } from 'discord.js'
+import { User } from 'discord.js'
 
-/* declare namespace Express {
-  export interface Request {
-    user?: DiscordJsUser
-  }
-} */
-
-/* declare module 'express' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface User extends DiscordJsUser {}
-} */
-
-declare global {
-  namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface User extends DiscordJsUser {}
+declare module 'express' {
+  interface Request {
+    user: User
   }
 }
